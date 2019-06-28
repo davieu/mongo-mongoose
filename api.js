a38c36b27eb72800ed0240d013a53dee9c0879aa
davieu <davisumana@gmail.com>
2019-06-27 21:05:59 -0400

    init commit

---
+++ api.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const { Schema } = mongoose;
+
+const app = express();
+
+//local 
+// mongoose.connect('mongodb://localhost/testTUT', {useNewUrlParser: true})
+mongoose.connect('mongodb+srv://davieu101:davieu101@cluster0-vgyon.mongodb.net/people?retryWrites=true&w=majority', {useNewUrlParser: true})
+
+// async function loadCollection() {
+//   const client = await mongoose.connect('mongodb+srv://davieu101:davieu101@cluster0-vgyon.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true})
+
+//   return client.db('mongo-mongoose-api').collection('people')
+// }
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+const UserSchema = new Schema({
+  lastname: String,
+  firstname: String
+});
+
+const User = mongoose.model('User', UserSchema ,'users')
+
+// const PersonModel = mongoose.model('poop', {
+//   lastname: String,
+//   firstname: String
+// });
+
+app.post('/api/person', async (req, res, next) => {
+  try {
+    let person = new User(req.body)
+    let result = await person.save();
+    res.send(result);
+
+  } catch(err) {
+    res.status(500).send(err)
+  }
+})
+
+// app.get('/api/people')
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.log(`Listening at port: ${PORT}`));
